6978b447e390e590786634303e77a1bbbab52e78
tigerwash <62541074+tigerwash@users.noreply.github.com>
2020-05-05 22:49:33 -0700

    Add files via upload

---
+++ labs/lab00/lab00.py
@@ -0,0 +1,17 @@
+def twenty_nineteen():
+    """Come up with the most creative expression that evaluates to 2019,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_nineteen()
+    2019
+    """
+    return 2018 + 1
+
+#def absoulte_value(x):
+    if x < 0:
+        return -x
+    elif x == 0:
+        return 0
+    else:
+        return x
+1 + 5
