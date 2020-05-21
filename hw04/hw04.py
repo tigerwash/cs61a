a16914bfd52c6c594bbd64d123c1de72317ab734
tigerwash <62541074+tigerwash@users.noreply.github.com>
2020-05-21 01:27:49 -0700

    Update hw04.py

--- hw04/hw04.py
+++ hw04/hw04.py
@@ -133,6 +133,25 @@ def count_change(amount):
         return count_help(total - coin, coin) + count_help(total, coin*2)
     return count_help(amount, 1)
 
+    #help function from count_partitions 
+    '''def count_partitions(n, m):
+    """Count the partitions of n using parts up to size m.
+
+    >>> count_partitions(6, 4)
+    9
+    >>> count_partitions(10, 10)
+    42
+    """
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    elif m == 0:
+        return 0
+    else:
+        with_m = count_partitions(n-m, m)
+        without_m = count_partitions(n, m-1)
+        return with_m + without_m'''
 
 
 def print_move(origin, destination):
